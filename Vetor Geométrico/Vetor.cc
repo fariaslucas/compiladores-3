a95e263f6ea1628c0d6c0403ad5c0fe35e4543b3
Lucas Farias <lukz@dcc.ufrj.br>
2021-12-29 18:29:45 -0300

    finalizando a construção do vetor

--- Vetor Geométrico/Vetor.cc
+++ Vetor Geométrico/Vetor.cc
@@ -7,26 +7,106 @@ using namespace std;
 template<int n, typename T>
 class Vetor {
     public:
-        class MeioDaAtribuicao {
+        class MeioDasOperacoes {
             private:
-                Vetor<n,T> *vetorAtr;
-                MeioDaAtribuicao(Vetor<n,T> *v): vetorAtr(v) {}
+                Vetor<n,T> *vetorOp;
+                MeioDasOperacoes(Vetor<n,T> *v): vetorOp(v) {}
                 friend class Vetor;
 
             public:
-                MeioDaAtribuicao operator , (T valor) {
-                    *vetorAtr = valor;
+                MeioDasOperacoes operator , (T valor) {
+                    *vetorOp = valor;
                     return *this;
                 }
         };
 
-        MeioDaAtribuicao operator = (initializer_list<T> valores) {
-            for (auto elemento : valores)
-                vetor.push_back(elemento);
+        void insere(T valor) {
+            vetor.push_back(valor);
+        }
 
-            return MeioDaAtribuicao(this);
+        MeioDasOperacoes operator = (initializer_list<T> valores) {
+            for (T valor : valores)
+                insere(valor);
+
+            return MeioDasOperacoes(this);
+        }
+
+        const void print(ostream& o) {
+            for (T valor : vetor)
+                o << valor << " ";
+        }
+
+        Vetor<n,T> operator + (const Vetor<n,T>& v) {
+            Vetor<n,T> soma;
+            for (unsigned int i = 0; i < n; i++)
+                soma.insere(vetor[i] + v.vetor[i]);
+            
+            return soma;
+        }
+
+        Vetor<n,T> operator - (const Vetor<n,T>& v) {
+            Vetor<n,T> resto;
+            for (unsigned int i = 0; i < n; i++)
+                resto.insere(vetor[i] - v.vetor[i]);
+            
+            return resto;
+        }
+
+        T operator * (const Vetor<n,T>& v) {
+            T produto = 0;
+            for (unsigned int i = 0; i < n; i++)
+                produto += vetor[i] * v.vetor[i];
+
+            return produto;
+        }
+
+        Vetor<n,T> operator * (T escalar) {
+            Vetor<n,T> produto;
+            for (unsigned int i = 0; i < n; i++)
+                produto.insere(vetor[i] * escalar);
+
+            return produto;
+        }
+
+        Vetor<n,T> operator / (T escalar) {
+            Vetor<n,T> quociente;
+            for (unsigned int i = 0; i < n; i++)
+                quociente.insere(vetor[i] / escalar);
+
+            return quociente;
+        }
+
+        MeioDasOperacoes operator * () {
+            return MeioDasOperacoes(this);
+        }
+
+        Vetor<n,T> operator * (const MeioDasOperacoes& meio) {
+            Vetor<n,T> produto;
+
+            if (n == 2) {
+                produto.insere(0);
+                produto.insere(0);
+            }
+            else if (n == 3) {
+                produto.insere(vetor[1] * (meio.vetorOp)->vetor[2] - vetor[2] * (meio.vetorOp)->vetor[1]);
+                produto.insere(vetor[2] * (meio.vetorOp)->vetor[0] - vetor[0] * (meio.vetorOp)->vetor[2]);
+            }
+            produto.insere(vetor[0] * (meio.vetorOp)->vetor[1] - vetor[1] * (meio.vetorOp)->vetor[0]);
+
+            return produto;
         }
 
     private:
         vector<T> vetor;
 };
+
+template<int n, typename T>
+Vetor<n,T> operator * (T escalar, Vetor<n,T> vetor) {
+    return vetor * escalar;
+}
+
+template<int n, typename T>
+ostream& operator << (ostream& o, Vetor<n,T> vetor) {
+    vetor.print(o);
+    return o;
+}
