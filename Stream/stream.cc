87984d6fd69f33bdbe57c70404e6a82d1826ac27
Lucas Farias <lukz@dcc.ufrj.br>
2022-01-04 22:51:34 -0300

    consertando stream.cc

--- Stream/stream.cc
+++ Stream/stream.cc
@@ -4,7 +4,6 @@ using namespace std;
 
 template<typename tipoItr, typename tipoFunc>
 void operator | (const tipoItr& iteravel, tipoFunc funcao) {
-    for( auto itr = v.cbegin(); itr != v.cend(); ++itr ) 
-        funcao(*itr);
-
+    for (auto elemento : iteravel)
+        funcao(elemento);
 }
