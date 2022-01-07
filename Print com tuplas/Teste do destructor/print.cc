2ebb310ca1aaf4b361c0a5b80f00dbc73d771b35
Lucas Farias <lukz@dcc.ufrj.br>
2022-01-06 21:10:36 -0300

    adicionando o teste do destructor

---
+++ Print com tuplas/Teste do destructor/print.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <initializer_list>
+#include <memory>
+
+using namespace std;
+
+class AbstractPair {
+    public:
+        virtual void imprime(ostream& o) = 0;
+};
+
+template<typename A, typename B>
+class PairImpl: public AbstractPair {
+    public:
+        PairImpl(A& valor1, B& valor2): primeiro(valor1), segundo(valor2) {}
+
+        void imprime(ostream& o) {
+            o << primeiro << " = " << segundo << endl;
+        }
+    
+    private:
+        A primeiro;
+        B segundo;
+};
+
+class Pair {
+    public:
+        template <typename A, typename B>
+        Pair(A primeiro, B segundo): p(shared_ptr<AbstractPair>{new PairImpl<A,B>(primeiro,segundo)}) {}
+
+        void imprime(ostream& o) {
+            p->imprime(o);
+        }
+
+    private:
+        shared_ptr<AbstractPair> p;
+};
+
+void print(ostream& o, initializer_list<Pair> lista) {
+    for (Pair elemento : lista) {
+        elemento.imprime(o);
+    }
+}
